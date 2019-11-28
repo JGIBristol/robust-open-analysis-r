2a2529df2c535c97b40268c3b3a5511993b43fb9
Natalie Thurlby <Natalie.thurlby@bristol.ac.uk>
2019-11-28 08:56:35 +0000

    Updated resources;

---
+++ assets/scripts/simulation_template.R
@@ -0,0 +1,7 @@
+# Experiment: how does changing the number of p-values we test increase 
+# our chance of a finding at least one "significant" result?
+# We'll create the code for 1 test together.
+
+# Define number of reps, significance threshold, and number of tests
+
+# Calculate the number below the significance threshold.
